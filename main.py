11866ccff9613712fb32d6cfe3e90bd449b5b953
Houda Aynaou <ouda_12@hotmail.fr>
2024-02-12 16:43:16 -0800

    Implement Encrypt Decrypt in fastapi

---
+++ main.py
@@ -0,0 +1,63 @@
+import base64
+import os
+from typing import Union
+
+from fastapi import FastAPI
+from google.cloud import kms, kms_v1
+from pydantic import BaseModel
+
+KMS_KEY_NAME = os.environ["KMS_KEY_NAME"]
+
+
+class EncryptRequest(BaseModel):
+    dek: str
+    authorized_users: list[str]
+
+
+class EncryptResponse(BaseModel):
+    envelope_header: str
+
+
+class DecryptRequest(BaseModel):
+    envelope_header: str
+
+
+class DecryptResponse(BaseModel):
+    dek: str
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/encrypt")
+def encrypt(request: EncryptRequest) -> EncryptResponse:
+    client = kms.KeyManagementServiceClient()
+    plaintext = request.model_dump_json().encode()
+    kms_response = client.encrypt(
+        request={
+            "name": KMS_KEY_NAME,
+            "plaintext": plaintext,
+        }
+    )
+    ciphertext_encoded = base64.b64encode(kms_response.ciphertext)
+    return EncryptResponse(envelope_header=ciphertext_encoded)
+
+
+@app.post("/decrypt")
+def decrypt(request: DecryptRequest) -> DecryptResponse:
+    client = kms.KeyManagementServiceClient()
+    ciphertext = base64.b64decode(request.envelope_header)
+    kms_response = client.decrypt(
+        request={
+            "name": KMS_KEY_NAME,
+            "ciphertext": ciphertext,
+        }
+    )
+    envelope = EncryptRequest.model_validate_json(kms_response.plaintext)
+    # TODO: Validate that the user is allowed to decrypt
+    return DecryptResponse(dek=envelope.dek)
