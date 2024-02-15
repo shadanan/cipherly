753ca4aebb0717d5ab11612785a336d624173729
Houda Aynaou <ouda_12@hotmail.fr>
2024-02-15 13:14:01 -0800

    Add encrypt and text boxes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,17 +1,53 @@
 <script lang="ts">
-  let display = "loading";
+  let secret = "";
+  let authorizedUsers = "";
+  let promise: Promise<any> | null = null;
 
-  async function load() {
-    const resp = await fetch("/api/hello");
-    const json = await resp.json();
-    display = json;
+  async function encrypt() {
+    const body = {
+      dek: "123456",
+      authorized_users: ["user1@gmail.com", "user2@gmail.com"],
+    };
+
+    const res = await fetch("/api/encrypt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const encryptResponse = await res.json();
+
+    if (res.ok) {
+      return encryptResponse["envelope_header"];
+    } else {
+      throw new Error(encryptResponse["envelope_header"]);
+    }
+  }
+
+  function handleClick() {
+    promise = encrypt();
   }
 </script>
 
 <main>
-  <button on:click={() => load()}>Load</button>
+  <input bind:value={secret} placeholder="Enter your secret here" />
+  <input
+    bind:value={authorizedUsers}
+    placeholder="Enter authorized users here"
+  />
+  <button on:click={handleClick}> Encrypt </button>
 
-  {JSON.stringify(display)}
+  {#if promise !== null}
+    {#await promise}
+      <p>Encrypting</p>
+    {:then number}
+      <p>{number}</p>
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+  {/if}
 </main>
 
 <style>
