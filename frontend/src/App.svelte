fa13f3b52c844b7373fb8b2bbcd57105ee43072c
Houda Aynaou <ouda_12@hotmail.fr>
2024-02-18 22:44:06 -0800

    Implement Decrypt

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,35 +1,50 @@
 <script lang="ts">
-  let secret = "";
-  let authorizedUsers = "";
-
   type Envelope = {
     header: string;
   };
 
-  let promise: Promise<String> | null = null;
+  type Dek = {
+    dek: { [key: string]: any };
+    iv: number[];
+  };
+
+  let secret = "";
+  let authorizedUsers = "";
+  let encryptedPayload = "";
+  let encryptpromise: Promise<String> | null = null;
+  let decryptpromise: Promise<String> | null = null;
 
   async function encrypt(
     secret: string,
     authorizedUsers: string[]
   ): Promise<String> {
+    // --------------------- PART 1 --------------------------
+    // 1. Generate a Data Encryption Key (dek) and Initialization Vector (iv)
     const dek = await window.crypto.subtle.generateKey(
       { name: "AES-GCM", length: 256 },
       true,
       ["encrypt", "decrypt"]
     );
+
     const iv = window.crypto.getRandomValues(new Uint8Array(12));
 
+    // 2. Encrypt the Secret using dek and iv
     const encryptedSecret = await window.crypto.subtle.encrypt(
       { name: "AES-GCM", iv: iv },
       dek,
       new TextEncoder().encode(secret)
     );
+
+    // 3. Encode the Encrypted Secret to a base64-encoded string
     const encodedSecret = btoa(
       String.fromCharCode(...new Uint8Array(encryptedSecret))
     );
 
+    // 4. Export the CryptoKey: dek to a portable format
     const encodedDek = await window.crypto.subtle.exportKey("jwk", dek);
 
+    // --------------------- PART 2 ---------------------
+    // Entypt encodedDek using kek (via server)
     const res = await fetch("/api/encrypt", {
       method: "POST",
       headers: {
@@ -41,9 +56,61 @@
         authorized_users: authorizedUsers,
       }),
     });
+    const envelopeEncrypted: Envelope = await res.json();
 
-    const envelope: Envelope = await res.json();
-    return envelope.header + "." + encodedSecret;
+    // --------------------- PART 3 ---------------------
+    // Result encrypted payload is the kek encrypted envelope + encodedSecret
+    return envelopeEncrypted.header + "." + encodedSecret;
+  }
+
+  async function decrypt(encryptedPayload: string[]): Promise<String> {
+    const envelopeEncryptedHeader = encryptedPayload[0];
+    const encodedSecret = encryptedPayload[1];
+
+    // --------------------- PART 1 ---------------------
+    // Decrypt encoded Dek using kek (via server)
+    const res = await fetch("/api/decrypt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        header: envelopeEncryptedHeader,
+      }),
+    });
+    const envelopeDecrypted: Dek = await res.json();
+
+    // --------------------- PART 2 ---------------------
+    // Encode dek, iv, encodedSecret into the correct format before decrypting
+    const ivBuffer = new Uint8Array(envelopeDecrypted.iv);
+
+    const cryptoKey = await window.crypto.subtle.importKey(
+      "jwk",
+      envelopeDecrypted.dek,
+      {
+        name: "AES-GCM",
+        length: 256,
+      },
+      true,
+      envelopeDecrypted.dek.key_ops
+    );
+
+    const decodedSecret = new Uint8Array(
+      atob(encodedSecret)
+        .split("")
+        .map((char) => char.charCodeAt(0))
+    );
+
+    // --------------------- PART 3 ---------------------
+    // Decrypt secret using cryptoKey and iv
+    const decryptedSecret = await window.crypto.subtle.decrypt(
+      { name: "AES-GCM", iv: ivBuffer },
+      cryptoKey,
+      decodedSecret
+    );
+
+    const secretDecrypted = new TextDecoder().decode(decryptedSecret);
+    return secretDecrypted;
   }
 </script>
 
@@ -59,16 +126,40 @@
   </div>
   <div>
     <button
-      on:click={() => (promise = encrypt(secret, authorizedUsers.split(",")))}
+      on:click={() =>
+        (encryptpromise = encrypt(secret, authorizedUsers.split(",")))}
       >Encrypt</button
     >
   </div>
   <div>
-    {#if promise !== null}
-      {#await promise}
+    {#if encryptpromise !== null}
+      {#await encryptpromise}
         <p>Encrypting</p>
       {:then payload}
         <p>{payload}</p>
+      {:catch error}
+        <p style="color: red">{error.message}</p>
+      {/await}
+    {/if}
+  </div>
+  <div>
+    <input
+      bind:value={encryptedPayload}
+      placeholder="Enter your Encrypted text here"
+    />
+  </div>
+  <div>
+    <button
+      on:click={() => (decryptpromise = decrypt(encryptedPayload.split(".")))}
+      >Decrypt</button
+    >
+  </div>
+  <div>
+    {#if decryptpromise !== null}
+      {#await decryptpromise}
+        <p>Decrypting</p>
+      {:then Encyptedtext}
+        <p>{Encyptedtext}</p>
       {:catch error}
         <p style="color: red">{error.message}</p>
       {/await}
