5d75fdc53f955f6b6edf72ddd49ff834cdf13619
Shad Sharma <shadanan@gmail.com>
2024-02-23 11:08:16 -0800

    Configure FastAPI to serve compiled SvelteKit app (#9)
    - Migrate to SvelteKit to take advantage of filesystem based routes
    - Configure FastAPI to mount built SPA at /*

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [sveltekit()],
   server: {
     proxy: {
       "/api": {
@@ -13,4 +12,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
