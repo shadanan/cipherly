cac7d9f01bd998a0569755718a5eb67829c923f2
Shad Sharma <shadanan@gmail.com>
2024-04-05 13:22:05 -0700

    Use prettier-plugin-organize-imports instead of prettier-plugin-sort-imports (#29)

--- frontend/src/lib/components/ui/button/index.ts
+++ frontend/src/lib/components/ui/button/index.ts
@@ -1,6 +1,6 @@
-import Root from "./button.svelte";
 import type { Button as ButtonPrimitive } from "bits-ui";
 import { tv, type VariantProps } from "tailwind-variants";
+import Root from "./button.svelte";
 
 const buttonVariants = tv({
   base: "inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -39,12 +39,12 @@ type Props = ButtonPrimitive.Props & {
 type Events = ButtonPrimitive.Events;
 
 export {
-  Root,
-  type Props,
-  type Events,
   //
   Root as Button,
-  type Props as ButtonProps,
-  type Events as ButtonEvents,
   buttonVariants,
+  Root,
+  type Events as ButtonEvents,
+  type Props as ButtonProps,
+  type Events,
+  type Props,
 };
