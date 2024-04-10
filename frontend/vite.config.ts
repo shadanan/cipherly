4daf6dace849f1812adbd299a18de9bcea3fc42b
Shad Sharma <shadanan@gmail.com>
2024-04-09 22:24:38 -0700

    Add frontend unit tests (vitest)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [sveltekit()],
@@ -11,5 +11,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
   },
 });
