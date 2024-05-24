d31d98c036a3cb2040e615d3e3d82a345e5f4d78
Shad Sharma <shadanan@gmail.com>
2024-05-23 22:08:13 -0700

    Try and fix the flash of huge Cipherly logo (#48)

--- frontend/src/lib/components/Banner.svelte
+++ frontend/src/lib/components/Banner.svelte
@@ -6,9 +6,7 @@
   <div class="mb-8 flex w-full flex-col items-center space-y-4 text-center">
     <a href="/">
       <div class="flex items-center space-x-2">
-        <div class="w-10 h-10">
-          <img alt="The project logo" src={cipherlyLogo} class="w-10" />
-        </div>
+        <img alt="The project logo" src={cipherlyLogo} width="40px" />
         <div class="text-4xl font-extrabold uppercase">Cipherly</div>
       </div>
     </a>
